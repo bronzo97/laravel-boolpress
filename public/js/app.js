822c3ce6e7af2557f5f9f29dadfed1cf5bd7af9a
bronzo97 <danilobronzo97@hotmail.it>
2022-08-24 18:49:13 +0200

    init

--- public/js/app.js
+++ public/js/app.js
@@ -49653,8 +49653,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/danilobronzo/Documents/Boolean/laravel/laravel-one-to-many/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/danilobronzo/Documents/Boolean/laravel/laravel-one-to-many/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/danilobronzo/Documents/Boolean/laravel/laravel-one-to-many-1/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/danilobronzo/Documents/Boolean/laravel/laravel-one-to-many-1/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
